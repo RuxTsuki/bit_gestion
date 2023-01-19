e26947224d9abf54ce0adb21ed2f5b1bc8f20417
SaiKisho <diegosferas@gmail.com>
2023-01-18 20:41:11 -0500

    Refactor custom hook for inventory grid context

--- src/contexts/dataGrids/inventoryGrid/hooks/useInventoryGrid.ts
+++ src/contexts/dataGrids/inventoryGrid/hooks/useInventoryGrid.ts
@@ -1,4 +1,20 @@
+import { useContext } from "react";
+import { InventoryGridContext, InventoryGridDispatch } from './../inventoryGridContext';
 
-export const useInventoryGrid = () => {
+export const useInventoryGridState = () => {
+    const context = useContext(InventoryGridContext);
 
-}
+    if (!context)
+        throw new Error('InventoryGridContext must be used within a InventoryGridProvider');
+
+    return context;
+};
+
+export const useInventoryGridDispatch = () => {
+    const context = useContext(InventoryGridDispatch);
+
+    if (!context)
+        throw new Error('InventoryGridDispatch must be used within a InventoryGridProvider');
+
+    return context;
+};

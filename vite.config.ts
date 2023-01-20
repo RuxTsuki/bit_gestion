6b22397d653d13c485014015b5809917e5067c03
SaiKisho <diegosferas@gmail.com>
2023-01-20 02:04:25 -0500

    Vite Base Url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bit_gestion/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   },

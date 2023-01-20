7cd6f5ebc6323adfaa108ba54075537a750936fa
SaiKisho <diegosferas@gmail.com>
2023-01-19 23:45:53 -0500

    Refactor routing go to login if not auth

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,20 @@
 import { Sidebar } from '@/ui/molecules/Sidebar'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import './dashboard.css';
+import { useAuthState } from '@/contexts/auth/hooks/useAuthContext';
+import { AuthStateType } from '@/contexts/auth/auth.types';
+import { useEffect } from 'react';
 
 export const Dashboard = () => {
+    const { state } = useAuthState();
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+        if (state === AuthStateType.notAuthenticated) {
+            navigateTo('/login');
+        }
+    }, []);
+
     return (
         <div className='dashboard-container'>
             <Sidebar />
